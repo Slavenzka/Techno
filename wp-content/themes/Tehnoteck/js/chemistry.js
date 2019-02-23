27dc6f41d59b33bb12cc636899399f995c500214
slavenzka <proekt.energetika@gmail.com>
2019-02-23 20:39:29 +0300

    Разметил и стилизовал страницу реагентов

---
+++ wp-content/themes/Tehnoteck/js/chemistry.js
@@ -0,0 +1,74 @@
+(function () {
+  'use strict';
+
+  let operateScheme = new function () {
+    let body = document.querySelector('body');
+    let list = document.querySelector('.reagents__list');
+    let items = list.querySelectorAll('.reagents__item');
+    let modalList = list.querySelectorAll('.mod__wrapper');
+    var hasScrollbar = window.innerWidth > document.documentElement.clientWidth;
+
+    function clearActiveModal () {
+      let activeModal = body.querySelector('.mod__wrapper--opened');
+      if (activeModal) {
+        activeModal.style.animationName = 'hideup';
+        activeModal.addEventListener('animationend', function () {
+          activeModal.remove();
+        });
+        if (hasScrollbar) {
+          body.classList.remove('body--pad');
+        }
+        body.classList.remove('body--block');
+      }
+    }
+
+    function showModal (elem, i) {
+      let modal = modalList[i].cloneNode(true);
+      let btn = modal.querySelector('.mod__close');
+
+      function handleClickBtn () {
+        clearActiveModal();
+        btn.removeEventListener('click', handleClickBtn);
+      }
+
+      function hangleMissClick (evt) {
+        if (evt.target.classList.contains('mod__wrapper')) {
+          clearActiveModal();
+          document.removeEventListener('click', hangleMissClick);
+        }
+      }
+
+      function handleEscPressed (evt) {
+        const escKey = 27;
+
+        if (evt.keyCode === escKey) {
+          clearActiveModal();
+          document.removeEventListener('keydown', handleEscPressed);
+        }
+      }
+
+      body.appendChild(modal);
+      modal.classList.add('mod__wrapper--opened');
+      modal.style.animationName = "dropdown";
+      body.classList.add('body--block');
+      if (hasScrollbar) {
+        body.classList.add('body--pad');
+      }
+
+      btn.addEventListener('click', handleClickBtn);
+      document.addEventListener('keydown', handleEscPressed);
+      document.addEventListener('click', hangleMissClick);
+    }
+
+    this.toggleModal = function () {
+      items.forEach((item, index) => {
+        item.addEventListener('click', function (evt) {
+          clearActiveModal();
+          showModal(item, index);
+        });
+      });
+    }
+  }
+
+  operateScheme.toggleModal();
+})();
